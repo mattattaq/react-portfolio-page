07f7467079425d8c2d555087f7b83bfad4fbfa06
Matthew Allbright <matthew.allbright@rooksecurity.com>
2017-08-03 02:36:41 -0400

    finished cleaning things up, I think you've got it from here.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, IndexRoute } from 'react-router-dom';
-
-import Nav from './components/Nav';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-
-
-ReactDOM.render((
-  <BrowserRouter>
-    <div>
-        <IndexRoute component={ Home } />
-        <Route exact path='/About' component={ About } />
-        <Route exact path='/Projects' component={ Projects } />
-        <Route exact path='/Contact' component={ Contact } />
-        <Route path='*' component={ Home } />
-    </div>
-  </BrowserRouter>
+import { BrowserRouter as Router,
+  Route, Link, browserHistory
+ } from 'react-router-dom';
+ import Home from './components/Home';
+ import About from './components/About';
+  ReactDOM.render(
+    <Router history={browserHistory} >
+      <div>
+        {/*load navBar here*/}
+        <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+      <hr/>
+      
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      </div>
+    </Router>,
+    document.getElementById('app')
   )
-, document.getElementById('app'));
+
+export default App;
